5ac107f0f884d102252a2417350f3830be5b0b92
amchvear <161626020+amchvear@users.noreply.github.com>
2024-10-09 21:11:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function toggleDetails() {
-    const moreInfo = document.getElementById("more-info");
-    if (moreInfo.classList.contains("hidden")) {
-        moreInfo.classList.remove("hidden");
+    const moreInfo = document.getElementById('more-info');
+    if (moreInfo.classList.contains('hidden')) {
+        moreInfo.classList.remove('hidden');
     } else {
-        moreInfo.classList.add("hidden");
+        moreInfo.classList.add('hidden');
     }
 }
