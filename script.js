6fe14d4e0b22339932d5aa15a853b026e9e72415
amchvear <161626020+amchvear@users.noreply.github.com>
2024-10-09 21:32:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById('learnMoreBtn').addEventListener('click', function() {
     alert("Megatron is equipped with state-of-the-art technology to track light sources and navigate accordingly. Stay tuned for more updates!");
 });
+
